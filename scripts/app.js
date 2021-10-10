4f07263fe5bbd93fce920f100b3557fcf16429b3
Mariana Lozynska <marianalozynska@YPs-iMac.local>
2021-10-10 19:11:52 +0100

    moveBlockRight function

--- scripts/app.js
+++ scripts/app.js
@@ -3,24 +3,21 @@
 // / Create score variable and score box to show the result.
 // / Create level box
 // / How blocks are dropping down - setTimeout? 1s are chanching on 0, bottom 0s are changing on 1. setTimeout in setTimeout.
-
 // / How to detect collision with bottom row? fuctionn to check if is possible to move down.
 // / How to stop (freeze) the piece when it is on the bottom? function check if it is bottom line?
 // / how to detect freezed pices and stop moving the block.
-
-// * How to connect left/right moves to the keybord?
-// * How to detect collision with a wall, and to stop block to run out of the grid. functions canMoveRight? canMoveLeft?
+// / How to connect left/right moves to the keybord?
+// / How to detect collision with a wall, and to stop block to run out of the grid. functions canMoveRight? canMoveLeft?
 
 // * Create one block - use matrix.
 // * Create array with seven different shapes of blocks - 'tetriminos'
 // * Create array with seven different colours. Will choose them randomly.
-
 // * Random block appears on a top
 // *
 // *
 // * How to make the row to disappear? How to find that all cells of a row are filled in? All 1s?
+// * ROTATION
 // * How to connect rotation to the keybord?
-
 // * How to rotate near the wall?
 
 //? BONUS TASKS
@@ -45,9 +42,9 @@ function init() {
   // Create a Grid width = 12, height 20 - grid should refresh. setTimeout? function to create grid.
   // playfield - contains a picture of a grid on a timebeing. Every time when we change smth it is building new grid with changes.
   let playField = [
-    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -132,13 +129,13 @@ function init() {
   }
 
   // * ---> KEY CONTROL, FUNCTIONS <--- * //
-
+  // * // MOVE LEFT ***********************
   function canMoveLeft() {
     for (let y = gridRows - 1; y >= 0; y--) {
       for (let x = 0; x < gridColumns; x++) {
         if (playField[y][x] === 1) {
           // if block already moving on x===0, block can`t move left
-          if (x === 0) {
+          if (x === 0 || playField[y][x - 1] === 2) {
             return false
           }
         }
@@ -159,7 +156,33 @@ function init() {
       }
     }
   }
-
+  // * // MOVE RIGHT ***********************
+  function canMoveRight() {
+    for (let y = gridRows - 1; y >= 0; y--) {
+      for (let x = 0; x < gridColumns; x++) {
+        if (playField[y][x] === 1) {
+          // if block already moving on x===gridColumn -1, block can`t move left
+          if (x === gridColumns - 1 || playField[y][x + 1] === 2) {
+            return false
+          }
+        }
+      }
+    }
+    return true
+  }
+  function moveBlockRight() {
+    // Check if it is possible to move right - true or false
+    if (canMoveRight()) {
+      for (let y = gridRows - 1; y >= 0; y--) {
+        for (let x = gridColumns - 1; x >= 0; x--) {
+          if (playField[y][x] === 1) {
+            playField[y][x + 1] = 1
+            playField[y][x] = 0
+          }
+        }
+      }
+    }
+  }
   function handleKeyUp(event) {
     const key = event.keyCode
     if (key === 37) {
@@ -168,6 +191,7 @@ function init() {
       console.log('LEFT')
     } else if (key === 39) {
       // Move right
+      moveBlockRight()
       console.log('RIGHT')
     } else if (key === 40) {
       // Move down
